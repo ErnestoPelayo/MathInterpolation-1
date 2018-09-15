3cc32cdc402225d14ac6baeceb2a4deca47094fb
Ernesto Pelayo <erneto.p@itmexicali.edu.mx>
2018-09-15 16:41:54 -0700

    added method to cubic spline

--- com/demo/interpol/TestMethodsInterpolation.java
+++ com/demo/interpol/TestMethodsInterpolation.java
@@ -15,8 +15,32 @@ public class TestMethodsInterpolation {
         yy.addElement(7.0);
         Lagrange instance = new Lagrange();
         double expResult = 7.0;
-        double result = instance.calculateResult(t, xx, yy);
+        double result = instance.testLagrange(t, xx, yy);
         assertEquals(expResult, result, 0.0);
+
+    }
+
+    @Test
+    public void testCubicSpline() {
+        double t = 2;
+        Vector<Double> xx = new Vector<Double>();
+        xx.addElement(1.1);
+        xx.addElement(2.2);
+        xx.addElement(3.3);
+        xx.addElement(4.4);
+
+
+        Vector<Double> yy = new Vector<Double>();
+        yy.addElement(1.1);
+        yy.addElement(2.2);
+        yy.addElement(3.3);
+        yy.addElement(4.4);
+
+        CubicSpline instance = new CubicSpline();
+        double expResult = 2.0;
+        double result = instance.testCubicSpline(t, xx, yy);
+        assertEquals(expResult, result, 0.0);
+
 
     }
 
