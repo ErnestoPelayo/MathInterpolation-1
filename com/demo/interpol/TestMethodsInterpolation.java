d4adf35036f265271a9ba06e99a986205e2a2e70
Ernesto Pelayo <erneto.p@itmexicali.edu.mx>
2018-09-15 16:10:54 -0700

    add test to method langrange

--- com/demo/interpol/TestMethodsInterpolation.java
+++ com/demo/interpol/TestMethodsInterpolation.java
@@ -1,4 +1,23 @@
 package com.demo.interpol;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestMethodsInterpolation {
+
+    @Test
+    public void testLagrange() {
+        double t = 9;
+        Vector<Double> xx =new Vector<Double> ();
+        xx.addElement(6.0);
+        Vector<Double> yy = new Vector<Double>();
+        yy.addElement(7.0);
+        Lagrange instance = new Lagrange();
+        double expResult = 7.0;
+        double result = instance.calculateResult(t, xx, yy);
+        assertEquals(expResult, result, 0.0);
+
+    }
+
 }
