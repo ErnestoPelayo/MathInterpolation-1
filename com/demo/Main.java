8e04ed0f1e4c441e5a6910dc71801fb51d5db900
Ernesto Pelayo <erneto.p@itmexicali.edu.mx>
2018-09-15 16:02:52 -0700

    added class test

--- com/demo/Main.java
+++ com/demo/Main.java
@@ -5,17 +5,17 @@ import com.demo.interpol.Lagrange;
 
 public class Main {
 
-	public static void main(String[] args) {
-		InterpolationHelper helper;
-		
-		if(args.length == 0 || args.length < 2) {
-			System.out.println("Debe ejecutarse: $ java Interpol pathFileName value");
-			return;
-		} else {
-			helper = new InterpolationHelper(args[0], Double.parseDouble(args[1]), new Lagrange());
-			helper.getResult();
-			helper = new InterpolationHelper(args[0], Double.parseDouble(args[1]), new CubicSpline());
-			helper.getResult();
-		}
-	}
+    public static void main(String[] args) {
+        InterpolationHelper helper;
+
+        if (args.length == 0 || args.length < 2) {
+            System.out.println("Debe ejecutarse: $ java Interpol pathFileName value");
+            return;
+        } else {
+            helper = new InterpolationHelper(args[0], Double.parseDouble(args[1]), new Lagrange());
+            helper.getResult();
+            helper = new InterpolationHelper(args[0], Double.parseDouble(args[1]), new CubicSpline());
+            helper.getResult();
+        }
+    }
 }
---
+++ com/demo/interpol/TestMethodsInterpolation.java
@@ -0,0 +1,4 @@
+package com.demo.interpol;
+
+public class TestMethodsInterpolation {
+}
